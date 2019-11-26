541a6455b790495299a3cd7b21aefb4a3937f0eb
andreov <58147517+andreov@users.noreply.github.com>
2019-11-26 21:49:47 +0300

    Create Dangerfile

---
+++ Dangerfile.swift
@@ -0,0 +1,18 @@
+import Danger
+
+let danger = Danger()
+let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
+
+let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+let sourceChanges = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
+
+if !changelogChanged && sourceChanges != nil {
+  warn("No CHANGELOG entry added.")
+}
+
+// You can use these functions to send feedback:
+message("Highlight something in the table")
+warn("Something pretty bad, but not important enough to fail the build")
+fail("Something that must be changed")
+
+markdown("Free-form markdown that goes under the table, so you can do whatever.")
